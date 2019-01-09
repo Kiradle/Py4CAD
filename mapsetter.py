c2a4e593445d7d9897b17e80dd9336e402363501
Kiradle <kiradle@outlook.com>
2019-01-09 21:40:49 +0800

    got crossover point

--- mapsetter.py
+++ mapsetter.py
@@ -2,15 +2,11 @@ import os
 import sys
 import ezdxf
 import math
-import numpy as np
 import time
-
-
-
-
 
 # 加载当前路径
 sys.path.insert(0, os.path.abspath('..'))
+
 
 class Read(object):
     """docstring for XInsert"""
@@ -18,13 +14,13 @@ class Read(object):
     def __init__(self, dwg):
         # self.arg = arg
         self.dwg = dwg
-        self.modelspace = dwg.modelspace()
+        self.mspace = dwg.modelspace()
         self.entities = []
 
     # 获取insert基点
     def get_insert_info(self):
         i = 0
-        for insert in self.modelspace.query('INSERT'):
+        for insert in self.mspace.query('INSERT'):
             block = self.dwg.blocks[insert.dxf.name]
             for e in block:
                 # 对应Block包含"LINE"的Insert才能被记录下来
@@ -35,7 +31,7 @@ class Read(object):
                     rotation = insert.dxf.rotation
                     # Insert中line的起点、终点坐标
                     start = insert.dxf.insert[:2]
-                    end = cal_end(e.dxf.end[:2], rotation, start)
+                    end = cal_insert_end(e.dxf.end[:2], rotation, start)
                     self.set_entity_line(i, block_name, rotation, start, end)
 
     def set_entity_line(self, i, block_name, rotation, start, end):
@@ -54,20 +50,31 @@ class Read(object):
         self.entities.append(entity)
 
 
-def cal_end(b_end, rotation, start):
-
-    # 计算insert中LINE在modelspace的终点
+def cal_insert_end(b_end, rotation, start):
+    # 计算insert中LINE在mspace的终点
     ro = math.radians(rotation)
     sin = math.sin(ro)
     cos = math.cos(ro)
 
     # print('ro : ', rotation)
-    x_e = b_end[0] * cos - b_end[1] * sin + start[0]
-    y_e = b_end[0] * sin + b_end[1] * cos + start[1]
-    # print('bend : ', b_end)
-    # print('nend : ', int(x_e), int(y_e))
+    x = b_end[0] * cos - b_end[1] * sin + start[0]
+    y = b_end[0] * sin + b_end[1] * cos + start[1]
 
-    return x_e, y_e
+    return x, y
+
+
+def cal_block_point(point, rotation):
+    pass
+    # 计算crossover point在block中的坐标 可以与cal_insert_end合并
+    ro = math.radians(360 - rotation)
+    sin = math.sin(ro)
+    cos = math.cos(ro)
+
+    # print('ro : ', rotation)
+    x = point[0] * cos - point[1] * sin
+    y = point[0] * sin + point[1] * cos
+
+    return x, y
 
 
 def inSegment(p, linea, lineb):
@@ -75,12 +82,12 @@ def inSegment(p, linea, lineb):
     检查某交点是否在线段line上（不含line的端点），在求交点时已经确认两条直线不平行
     所以，对于竖直的line，lineb不可能竖直，却有可能水平，所以检查p是否在lineb上，只能检查x值即p[0]
     """
-    if linea[0][0] == linea[1][0]: # 如果line竖直
+    if linea[0][0] == linea[1][0]:  # 如果line竖直
         if min(linea[0][1], linea[1][1]) < p[1] < max(linea[0][1], linea[1][1]):
             if min(lineb[0][0], lineb[1][0]) <= p[0] <= max(lineb[0][0], lineb[1][0]):
                 return True
 
-    elif linea[0][1] == linea[1][1]  :  # 如果line水平
+    elif linea[0][1] == linea[1][1]:  # 如果line水平
         if min(linea[0][0], linea[1][0]) < p[0] < max(linea[0][0], linea[1][0]):
             if min(lineb[0][1], lineb[1][1]) <= p[1] <= max(lineb[0][1], lineb[1][1]):
                 return True
@@ -96,7 +103,7 @@ def inSegment(p, linea, lineb):
 
 
 def cal_crossover_point_v1(linea, lineb):
-    # 计算实际交点，快？
+    # 计算实际交点
     xa, ya = linea['start']
     xb, yb = linea['end']
     xc, yc = lineb['start']
@@ -104,9 +111,9 @@ def cal_crossover_point_v1(linea, lineb):
 
     try:
         x = ((xa * yb - ya * xb) * (xc - xd) - (xa - xb) * (xc * yd - yc * xd)) / (
-                    (xa - xb) * (yc - yd) - (ya - yb) * (xc - xd))
+                (xa - xb) * (yc - yd) - (ya - yb) * (xc - xd))
         y = ((xa * yb - ya * xb) * (yc - yd) - (ya - yb) * (xc * yd - yc * xd)) / (
-                    (xa - xb) * (yc - yd) - (ya - yb) * (xc - xd))
+                (xa - xb) * (yc - yd) - (ya - yb) * (xc - xd))
 
         if inSegment((x, y), ((xa, ya), (xb, yb)), ((xc, yc), (xd, yd))):
             return x, y
@@ -116,83 +123,33 @@ def cal_crossover_point_v1(linea, lineb):
         # 平行
         return None
 
-def cal_crossover_point_v2(linea ,lineb):
-    # 计算实际交点v2 ,慢？
-    xa, ya = linea['start']
-    xb, yb = linea['end']
-    xc, yc = lineb['start']
-    xd, yd = lineb['end']
 
-    # 判断两条直线是否相交，矩阵行列式计算
-    a = np.matrix(
-        [
-            [xb - xa, -(xd - xc)],
-            [yb - ya, -(yd - yc)]
-        ]
-    )
-    delta = np.linalg.det(a)
-    # 不相交,返回两线段
-    if np.fabs(delta) < 1e-6:
-        return None
-                      # 求两个参数lambda和miu
-    c = np.matrix(
-        [
-            [xc - xa, -(xd - xc)],
-            [yc - ya, -(yd - yc)]
-        ]
-    )
-    d = np.matrix(
-        [
-            [xb - xa, xc - xa],
-            [yb - ya, yc - ya]
-        ]
-    )
-    lamb = np.linalg.det(c) / delta
-    miu = np.linalg.det(d) / delta
-    # 相交
-    if 1 >= lamb >= 0 and 0 <= miu <= 1:
-        x = xc + miu * (xd - xc)
-        y = yc + miu * (yd - yc)
-        return x, y
-    # 相交在延长线上
-    else:
-        return None
-
-
-def crossover(reader):
-
-    while reader.entities:
-        insert = reader.entities.pop()
-        for line in reader.entities:
+def crossover(dwg_info):
+    # 循环取出最后一个线段与剩余的计算交点坐标
+    while dwg_info.entities:
+        insert = dwg_info.entities.pop()
+        for line in dwg_info.entities:
             if line['group'] != insert['group']:
 
                 point = cal_crossover_point_v1(insert, line)
 
-                if point :
-                    pass
+                if point:
+                    
+                    cp = (point[0] - insert['start'][0], point[1] - insert['start'][1])
+                    cpa = cal_block_point(cp, insert['rotation'])
+                    cp = (point[0] - line['start'][0], point[1] - line['start'][1])
+                    cpb = cal_block_point(cp, line['rotation'])
 
-                    print(insert['block_name'], line['block_name'], point)
+                    print(insert['block_name'], cpa, line['block_name'], cpb)
 
-                    # x = (point[0] - insert['start'][0])
-                    # y = (point[1] - insert['start'][1])
-                    #
-                    # print('xy ：： ', x, y)
-                    # # 需要追加rotation的问题
-                    # block = reader.dwg.blocks.get(insert['block_name'])
-                    # block.add_line((-2, y), (+2, y))
-                    #
-                    # x = (point[0] - line['start'][0])
-                    # y = (point[1] - line['start'][1])
-                    #
-                    # print('xy ：： ', x, y)
-                    # # 需要追加rotation的问题
-                    # block = reader.dwg.blocks.get(line['block_name'])
-                    # block.add_line(( - 25, y+50), ( + 25, y-50))
+                    block = dwg_info.dwg.blocks.get(insert['block_name'])
+                    block.add_line((-25, cpa[1] + 50), (+25, cpa[1] - 50))
+                    block = dwg_info.dwg.blocks.get(line['block_name'])
+                    block.add_line((- 25, cpb[1] + 50), (+ 25, cpb[1] - 50))
+
 
 if __name__ == '__main__':
     t1 = time.perf_counter()
-
-
 
     filepath = 'test1_2010.dxf'
     reader = Read(ezdxf.readfile(filepath))
@@ -200,8 +157,5 @@ if __name__ == '__main__':
     crossover(reader)
     reader.dwg.saveas('daaaa.dxf')
 
-
     t2 = time.perf_counter()
-    print(t2-t1)
-
-
+    print(t2 - t1)
